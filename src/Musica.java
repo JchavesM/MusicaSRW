1fe30baa791df3954a595f489e4a97b5c7b0a7c0
edkestebanpr23 <edkestebanpr23@gmail.com>
2020-02-16 14:59:15 -0500

    Soluciion - Interfaz

--- src/Musica.java
+++ src/Musica.java
@@ -17,6 +17,7 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
